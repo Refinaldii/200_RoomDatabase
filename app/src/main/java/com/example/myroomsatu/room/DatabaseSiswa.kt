742cecb961d774f20e893ef4f61b2d5bc1b4c493
Refinaldii <refinaldi9786@gmail.com>
2025-11-28 09:59:23 +0700

    Bikin companion object

--- app/src/main/java/com/example/myroomsatu/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/myroomsatu/room/DatabaseSiswa.kt
@@ -3,6 +3,7 @@ package com.example.myroomsatu.room
 @Database(entities = [Siswa::class], version = 1, exportSchema = false)
 abstract class DatabaseSiswa : RoomDatabase() {
     abstract fun siswaDao() : SiswaDao
+    companion object {
 }
 
 
