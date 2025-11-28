4dd362fee3d1f25cd16c4f246b8d373526a65eb5
Refinaldii <refinaldi9786@gmail.com>
2025-11-28 09:58:50 +0700

    Bikin refrensi ke siswa dao

--- app/src/main/java/com/example/myroomsatu/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/myroomsatu/room/DatabaseSiswa.kt
@@ -2,7 +2,7 @@ package com.example.myroomsatu.room
 
 @Database(entities = [Siswa::class], version = 1, exportSchema = false)
 abstract class DatabaseSiswa : RoomDatabase() {
-
+    abstract fun siswaDao() : SiswaDao
 }
 
 
